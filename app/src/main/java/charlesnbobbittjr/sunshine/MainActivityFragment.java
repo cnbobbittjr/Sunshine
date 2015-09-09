31e6909e1cce505f9270edc4a2f4d516431315e4
[Charles N Bobbitt Jr] <[cnbobbittjr@gmail.com]>
2015-09-08 18:47:27 -0600

    Starting point

--- app/src/main/java/charlesnbobbittjr/sunshine/MainActivityFragment.java
+++ app/src/main/java/charlesnbobbittjr/sunshine/MainActivityFragment.java
@@ -2,6 +2,7 @@ package charlesnbobbittjr.sunshine;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +10,19 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+{
 
-    public MainActivityFragment() {
+    public MainActivityFragment()
+    {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
+        //Log.e("Sunshine","test");
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
